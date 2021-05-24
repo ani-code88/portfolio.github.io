f29fe8a9a70971c5515d4d630382a7270ddcef69
Anil Swami <anilswami5388@gmail.com>
2021-05-24 10:45:17 +0530

    my team section updated

--- assets/StandupStartups/js/main.js
+++ assets/StandupStartups/js/main.js
@@ -27,45 +27,82 @@ $(function() {
 				/* submit via ajax */
 				submitHandler: function(form) {		
 					var $submit = $('.submitting'),
-						waitText = 'Submitting...';
+					waitText = 'Submitting...';
 
 					$.ajax({   	
-				      type: "POST",
-				      url: "php/send-email.php",
-				      data: $(form).serialize(),
+						type: "POST",
+						url: "php/send-email.php",
+						data: $(form).serialize(),
 
-				      beforeSend: function() { 
-				      	$submit.css('display', 'block').text(waitText);
-				      },
-				      success: function(msg) {
-		               if (msg == 'OK') {
-		               	$('#form-message-warning').hide();
-				            setTimeout(function(){
-		               		$('#contactForm').fadeOut();
-		               	}, 1000);
-				            setTimeout(function(){
-				               $('#form-message-success').fadeIn();   
-		               	}, 1400);
-			               
-			            } else {
-			               $('#form-message-warning').html(msg);
-				            $('#form-message-warning').fadeIn();
-				            $submit.css('display', 'none');
-			            }
-				      },
-				      error: function() {
-				      	$('#form-message-warning').html("Something went wrong. Please try again.");
-				         $('#form-message-warning').fadeIn();
-				         $submit.css('display', 'none');
-				      }
-			      });    		
-		  		}
+						beforeSend: function() { 
+							$submit.css('display', 'block').text(waitText);
+						},
+						success: function(msg) {
+							if (msg == 'OK') {
+								$('#form-message-warning').hide();
+								setTimeout(function(){
+									$('#contactForm').fadeOut();
+								}, 1000);
+								setTimeout(function(){
+									$('#form-message-success').fadeIn();   
+								}, 1400);
+
+							} else {
+								$('#form-message-warning').html(msg);
+								$('#form-message-warning').fadeIn();
+								$submit.css('display', 'none');
+							}
+						},
+						error: function() {
+							$('#form-message-warning').html("Something went wrong. Please try again.");
+							$('#form-message-warning').fadeIn();
+							$submit.css('display', 'none');
+						}
+					});    		
+				}
 				
 			} );
 		}
 	};
 	contactForm();
 
+	// ---------------------------------------------------------------
+	// Change Background Color Of Top Bar on Scroll 
+	// ---------------------------------------------------------------
+
+	window.onscroll = function() {changeTopBarBg()};
+	function changeTopBarBg() {
+		if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+			document.getElementById("top-logo-bar").classList.add("changeBgOnScroll");
+		}
+		else{
+			document.getElementById("top-logo-bar").classList.remove("changeBgOnScroll");
+		}
+	}
+
+	// ---------------------------------------------------------------
+	// Expand Team On View More Button Click 
+	// ---------------------------------------------------------------
 	
 
 });
+function expandTeam(btn) {
+		var rows = document.querySelectorAll(".team .row");
+		// var viewBtn = document.getElementById()
+		console.log(rows);
+		console.log(btn.innerHTML);
+		if(btn.innerHTML == "View More"){
+			for (let i = 1; i < rows.length; i++) {
+			rows[i].classList.remove("d-none");
+			btn.innerHTML = "View Less";
+		}
+		}
+		else{
+			btn.innerHTML = "View More";
+			for (let i = 1; i < rows.length; i++) {
+			rows[i].classList.add("d-none");
+			
+		}
+		
+	}
+}
